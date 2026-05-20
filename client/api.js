f1147ff72b686736e3be753e37bf4a8896e37885
kingofdead6 <ny_kahlouche@esi.dz>
2026-05-20 12:20:58 +0100

    Added new features

--- client/api.js
+++ client/api.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL="https://dms-crd4.onrender.com/api";
-export const API_BASE_URL="http://localhost:5000/api";
+export const API_BASE_URL="https://dms-crd4.onrender.com/api";
+// export const API_BASE_URL="http://localhost:5000/api";
