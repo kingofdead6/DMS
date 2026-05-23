cab38a6812e8a5609736db529fd119948e3900f3
kingofdead6 <ny_kahlouche@esi.dz>
2026-05-23 20:06:42 +0100

    Added new features

--- client/api.js
+++ client/api.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL="https://dms-crd4.onrender.com/api";
+export const API_BASE_URL="https://gym-protein.com/realbackenddms/api";
 // export const API_BASE_URL="http://localhost:5000/api";
